5783b624bc6b6f379e4b2c74a931adccf6900b8b
Manan Telrandhe <telrandhemanan@gmail.com>
2024-09-29 13:25:16 +0530

    native screens and wind addded

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import {StatusBar } from 'expo-status-bar';
+import {StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router'; 
+export default function App() {
+return (
+<View className="flex-1 items-center justify-center bg-white">
+<Text className="text-3xl font-bold">Welcome to Wristique !</Text>
+<StatusBar style="auto" />
+<Link href="/profile" style={{color:'blue'}}>Profile</Link>
+</View> 
+);
+}
