56c00736a671fa1557318159b1159d0b1059789d
Manan Telrandhe <telrandhemanan@gmail.com>
2024-09-29 19:19:37 +0530

    structure and dynamics setup

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Tablayout = () => {
+  return (
+    <View>
+      <Text>Tablayout
+    
+      </Text>
+    </View>
+  )
+}
+
+export default Tablayout
--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ return (
 <View className="flex-1 items-center justify-center bg-white">
 <Text className="text-3xl font-bold">Welcome to Wristique !</Text>
 <StatusBar style="auto" />
-<Link href="/profile" style={{color:'blue'}}>Profile</Link>
+<Link href="/home" style={{color:'blue'}}>Navigate To Home</Link>
 </View> 
 );
 }
