8eb4db4b0aa1ea58d2f49c1766a5a96b28495e14
Manan Telrandhe <telrandhemanan@gmail.com>
2024-10-01 20:33:46 +0530

    component and home screen added with expo-av

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,88 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, Image, RefreshControl, Text, View,TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+import { images } from "../../constants";
+import useAppwrite from "../../lib/useAppwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
+import EmptyState from "../../components/EmptyState.jsx";
+import SearchInput from "../../components/SearchInput.jsx";
+import VideoCard from "../../components/VideoCard.jsx";
+import Trending from "../../components/Trending.jsx";
 
 const home = () => {
-  return (
-    <View>
-      <Text>home</Text>
-    </View>
-  )
-}
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
-export default home
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
+  return (
+    <SafeAreaView className="flex-1 bg-black bg-gradient-to-b from-black via-blue-500 to-yellow-400">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex flex-row justify-between items-center mb-6">
+              <View>
+                <Text className="font-medium text-sm text-yellow-400">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-semibold text-white">
+                  Manan
+                </Text>
+              </View>
+
+              <TouchableOpacity
+                onPress={() => router.push("/")} 
+                className="mt-1.5"
+              >
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <SearchInput />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-regular text-yellow-400 mb-3">
+                Latest Videos
+              </Text>
+
+              <Trending posts={latestPosts ?? []} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default home;
